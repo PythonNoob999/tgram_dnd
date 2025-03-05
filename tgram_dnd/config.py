cfa9d9d53f2085482630c28106bbef5ef396a5a0
PythonNoob999 <kerolisand@gmail.com>
2025-03-05 23:14:19 +0200

    added jsonLoader

--- tgram_dnd/config.py
+++ tgram_dnd/config.py
@@ -4,6 +4,7 @@ from tgram_dnd.errors import InvalidStrings
 
 from tgram.types import (
     InlineKeyboardMarkup,
+    InlineKeyboardButton,
     BotCommand,
     Update
 )
@@ -16,15 +17,44 @@ import json
 class BotConfig:
     def __init__(
         self,
-        strings: Dict[str, Dict[LANGUAGE_CODES, str]],
+        strings: Dict[str, Dict[LANGUAGE_CODES, str]] = None,
         keyboards: Dict[str, Dict[LANGUAGE_CODES, InlineKeyboardMarkup]] = None,
         default_lang: LANGUAGE_CODES = "en",
-        bot_commands: List[BotCommandInput] = None
+        bot_commands: List[BotCommandInput] = None,
+        config_file: str = None
     ):
-        self.strings = strings
+        self.strings = strings or {}
         self.keyboards = keyboards or {}
         self.default_lang = default_lang
         self.commands = bot_commands or []
+        if os.path.isfile(config_file):
+            self.load_file_data(config_file)
+    
+    def load_file_data(
+        self,
+        file: str
+    ) -> None:
+        data = json.load(open(file, "r+", encoding="utf-8"))
+        self.strings = data.get("strings", {})
+        self.commands = data.get("commands", [])
+        self.default_lang = data.get("default_language", "en")
+
+        # loading keyboards
+        kbs = data.get("keyboards", {})
+        result: Dict[str, Dict[LANGUAGE_CODES, InlineKeyboardMarkup]] = {}
+        for kb in kbs:
+            result[kb] = {}
+            for klang in kbs[kb]:
+                result[kb][klang] = []
+                keyboard = kbs[kb][klang]
+                for rows in keyboard:
+                    result[kb][klang].append(
+                        [InlineKeyboardButton(
+                            **button
+                        ) for button in rows]
+                    )
+                result[kb][klang] = InlineKeyboardMarkup(result[kb][klang])
+        self.keyboards = result
 
     def load_strings(self) -> None:
         if isinstance(self.strings, str):
