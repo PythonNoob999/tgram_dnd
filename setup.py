7e8b678c87e1552cd1ee59148a02ecef4ee7954c
PythonNoob999 <kerolisand@gmail.com>
2025-03-28 06:47:28 +0200

    pumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tgram_dnd",
-    version="0.0.5beta",
+    version="0.0.6beta",
     description="Tgram Darg-And-Drop helper is a lib to help building DragAndDrop applications, or simply just to make bots faster",
     long_description=open("readme.md", "r+").read(),
     long_description_content_type='text/markdown',
@@ -11,7 +11,9 @@ setup(
         "tgram_dnd",
         "tgram_dnd.blocks",
         "tgram_dnd.actions",
-        "tgram_dnd.actions.methods",
+        "tgram_dnd.actions.api",
+        "tgram_dnd.actions.api.methods",
+        "tgram_dnd.caching",
         "tgram_dnd.enums",
         "tgram_dnd.flows"
     ],
